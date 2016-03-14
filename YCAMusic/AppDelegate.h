b1aada57b37a510cdc81d3443f6e6a5a833ef376
xyhang <929472555@qq.com>
2016-03-14 16:58:22 +0800

    Initial Commit

---
+++ YCAMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCAMusic
+//
+//  Created by yan on 16/3/14.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCAMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCAMusic
+//
+//  Created by yan on 16/3/14.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
